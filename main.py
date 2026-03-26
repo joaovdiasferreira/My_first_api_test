f7311ddde50ccd8987bc185b74109bf98ff74f00
ferreira <jvitordiasferreira@gmail.com>
2026-03-26 17:00:19 -0300

    ResponseModel and InputModel implemented | Dict Structured

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
+from typing import TypedDict
 app = FastAPI()
 
-#Model
+#ModelBase
 class Sale(BaseModel):
     product: str = Field(min_length=1, max_length=100)
     price: float = Field(gt=0, lt=100000)
     quantity: int = Field(gt=0, lt=1000)
     #Field helps to limit space usage or possible errors
 
+#RespondeModel or output model
+class SaleResponse(Sale): #inhirits everything from Sale
+    id: int
+    total: float
+
+#Input model
+class SaleCreate(Sale):
+    pass
+
+class SaleDict(TypedDict):
+    product: str
+    price: float
+    quantity: int
+    total: float
+
+
 #Fake database
-sales: dict[int, dict[str, float | int | str]] = { #This turns the dict a bit restrictive
+sales: dict[int, SaleDict] = { #This turns the dict a bit restrictive
     1: {
         "product": "Keyboard",
         "price": 150.0,
@@ -45,56 +62,62 @@ def calculate_total(price: float, quantity: int) -> float:
 
 
 
-#Routers
+#Routes
 @app.get("/")
 def home():
     return {"message": "My first API is running!"}
 
-@app.get("/sales")
+@app.get("/sales", response_model=list[SaleResponse])
 def get_sales():
-    return sales
+    result = []
+    for sale_id, sale in sales.items():
+        result.append({"id": sale_id, **sale})
+    return result
 
-@app.get("/sales/{sale_id}")
-def get_sale(sale_id: int) -> dict:
+@app.get("/sales/{sale_id}", response_model=SaleResponse)
+def get_sale(sale_id: int):
     sale = sales.get(sale_id)
-    if not sales:
+    if not sale:
         raise HTTPException(status_code=404, detail="Sale not found")
         #This prevents KeyError if a sale is not found
-    return sale
+    return {"id": sale_id, **sale}
 
 
 #CREAT a sale (POST)
-@app.post("/sales")
-def create_sale(sale: Sale):
+@app.post("/sales", response_model=SaleResponse)
+def create_sale(sale: SaleCreate):
     sale_id = max(sales.keys(), default=0) + 1 #this avoids that an id repeat
+    total = calculate_total(sale.price, sale.quantity)
 
     sales[sale_id]= {
         "product": sale.product,
         "price": sale.price,
         "quantity": sale.quantity,
-        "total": calculate_total(sale.price, sale.quantity)
+        "total": total
     }
-    return {"id": sale_id, "sale": sales[sale_id]}
+    return {"id": sale_id, **sales[sale_id]}
 
 #DELETE a sale
 @app.delete("/sales/{sale_id}")
 def delete_sale(sale_id: int):
     if sale_id not in sales:
         raise HTTPException(status_code=404, detail="Sale not found")
-    deleted = sales.pop(sale_id) #pop() returns what was deleted
-    #del sales[sale_id] is possible, but it don't returns anything
-    return {"message": "Sale deleted!", "sale": deleted}
+    sales.pop(sale_id) #pop() returns what was deleted
+    #del sales[sale_id] is possible, but it not returns anything
+    return {"id": sale_id, "message": "Sale deleted!"}
 
-@app.put("/sales/{sale_id}")
-def update_sale(sale_id: int, sale: Sale):
+
+#UPDATE a sale (PUT)
+@app.put("/sales/{sale_id}", response_model=SaleResponse)
+def update_sale(sale_id: int, sale: SaleCreate):
     if sale_id not in sales:
         raise HTTPException(status_code=404, detail="Sale not found")
+    total = calculate_total(sale.price, sale.quantity)
+
     sales[sale_id] = {
         "product": sale.product,
         "price": sale.price,
         "quantity": sale.quantity,
-        "total": calculate_total(sale.price, sale.quantity)
+        "total": total
     }
-    return {"message": "Sale updated!", "sale": sales[sale_id]}
-
-print(sales)
+    return {"id": sale_id, **sales[sale_id]}
